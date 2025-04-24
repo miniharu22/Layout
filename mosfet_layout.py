92fa383db44faad2c8480c957101c8df44004f0b
miniharu <134501746+miniharu22@users.noreply.github.com>
2025-04-24 12:21:09 +0900

    Update mosfet_layout.py

--- mosfet_layout.py
+++ mosfet_layout.py
@@ -4,44 +4,31 @@ layout = pya.Layout()
 mosfet = layout.create_cell("MOSFET")
 
 
-## Well Layer
+# Well Layer
 l1 = layout.layer(1,0)
 mosfet.shapes(l1).insert(pya.Box(800, 6750, 4800, 8330))
 
-## Active Layer
+# Active Layer
 l2 = layout.layer(2,0)
 mosfet.shapes(l2).insert(pya.Box(1500, 6850, 4000, 8230))
 
 
-## Gate Layer
+# Gate Layer
 l3 = layout.layer(3,0)
 mosfet.shapes(l3).insert(pya.Box(1500, 7450, 4000, 7630))
 
-## Contact Layer
+# Contact Layer
 l4 = layout.layer(4,0)
-mosfet.shapes(l4).insert(pya.Box(2000, 7780, 2300, 8080))
+mosfet.shapes(l4).insert(pya.Box(2000, 7780, 2300, 8080)) ## Drain Contact
 mosfet.shapes(l4).insert(pya.Box(2600, 7780, 2900, 8080))
 mosfet.shapes(l4).insert(pya.Box(3200, 7780, 3500, 8080))
 
-mosfet.shapes(l4).insert(pya.Box(2000, 7000, 2300, 7300))
+mosfet.shapes(l4).insert(pya.Box(2000, 7000, 2300, 7300)) ## Source Contact
 mosfet.shapes(l4).insert(pya.Box(2600, 7000, 2900, 7300))
 mosfet.shapes(l4).insert(pya.Box(3200, 7000, 3500, 7300))
 
-## Drain Layer 
-l5 = layout.layer(5,0)
-mosfet.shapes(l5).insert(pya.Box(2050, 7822, 2250, 8037))
-mosfet.shapes(l5).insert(pya.Box(2650, 7822, 2850, 8037))
-mosfet.shapes(l5).insert(pya.Box(3250, 7822, 3450, 8037))
-  
 
-## Source Layer 
-l6 = layout.layer(6,0)
-mosfet.shapes(l6).insert(pya.Box(2050, 7055, 2250, 7245))  
-mosfet.shapes(l6).insert(pya.Box(2650, 7055, 2850, 7245))  
-mosfet.shapes(l6).insert(pya.Box(3250, 7055, 3450, 7245))
-
-
-## Insert Text
+# Insert Text
 gen = pya.TextGenerator()
 
 text = gen.default_generator().text("MOSFET", 0.002)
